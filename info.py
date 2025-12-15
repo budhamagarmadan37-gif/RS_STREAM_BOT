1ee64f28ab4191f8e738593bfa8273b16a4dee58
RAHAT11790 <rahatsarker224@gmail.com>
2025-12-15 20:58:51 +0600

    Update info.py

--- info.py
+++ info.py
@@ -33,4 +33,4 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "rs_fllink_bot")
 # Shortlink Info
 SHORTLINK = "False" # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2'
+SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
