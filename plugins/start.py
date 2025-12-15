e65c62e786d1f6058ac541b4409ea65863e0abea
RAHAT11790 <rahatsarker224@gmail.com>
2025-12-15 23:08:50 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,70 +1,51 @@
-import random
-import humanize
-from Script import script
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-from info import URL, LOG_CHANNEL, SHORTLINK
+from TechVJ.bot import TechVJBot
+from pyrogram import filters
 from urllib.parse import quote_plus
+from info import URL, LOG_CHANNEL, SHORTLINK
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 from TechVJ.util.human_readable import humanbytes
-from database.users_chats_db import db
-from utils import temp, get_shortlink
+from utils import get_shortlink
 
-@Client.on_message(filters.command("start") & filters.incoming)
-async def start(client, message):
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id, message.from_user.first_name)
-        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
-    rm = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton("✨ Update Channel", url="https://t.me/vj_botz")
-        ]] 
-    )
-    await client.send_message(
-        chat_id=message.from_user.id,
-        text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-        reply_markup=rm,
-        parse_mode=enums.ParseMode.HTML
-    )
-    return
-
-
-@Client.on_message(filters.private & (filters.document | filters.video))
+@TechVJBot.on_message(filters.private & (filters.video | filters.document))
 async def stream_start(client, message):
-    file = getattr(message, message.media.value)
-    filename = file.file_name
-    filesize = humanize.naturalsize(file.file_size) 
-    fileid = file.file_id
-    user_id = message.from_user.id
-    username =  message.from_user.mention 
+
+    file = message.video or message.document
+    if not file:
+        return
+
+    status = await message.reply_text("⏳ Processing your file...")
 
     log_msg = await client.send_cached_media(
         chat_id=LOG_CHANNEL,
-        file_id=fileid,
+        file_id=file.file_id
     )
-    fileName = {quote_plus(get_name(log_msg))}
-    if SHORTLINK == False:
-        stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        download = f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-    else:
-        stream = await get_shortlink(f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}")
-        download = await get_shortlink(f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}")
-        
-    await log_msg.reply_text(
-        text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
-        quote=True,
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
-                                            InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
-    )
-    rm=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
-                InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ 📥", url=download)
-            ]
-        ] 
-    )
-    msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
 
-    await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(message)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)
+    file_name = get_name(log_msg)
+    size = humanbytes(get_media_file_size(message))
+
+    secure_hash = get_hash(log_msg)
+    msg_id = log_msg.id
+
+    if not SHORTLINK:
+        stream = f"{URL}watch/{secure_hash}{msg_id}"
+        download = f"{URL}{secure_hash}{msg_id}"
+    else:
+        stream = await get_shortlink(f"{URL}watch/{secure_hash}{msg_id}")
+        download = await get_shortlink(f"{URL}{secure_hash}{msg_id}")
+
+    buttons = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton("🖥 Watch Online", url=stream),
+            InlineKeyboardButton("📥 Download", url=download)
+        ]]
+    )
+
+    text = (
+        f"<b>✅ Your Link Generated!</b>\n\n"
+        f"<b>📂 File:</b> <i>{file_name}</i>\n"
+        f"<b>📦 Size:</b> <i>{size}</i>\n\n"
+        f"<b>🖥 Stream:</b> {stream}\n"
+        f"<b>📥 Download:</b> {download}"
+    )
+
+    await status.edit_text(text, reply_markup=buttons, disable_web_page_preview=True)
