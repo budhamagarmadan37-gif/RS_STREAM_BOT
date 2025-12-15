6d46b6cb905b0450fd88e3f8ca694e0467301758
RAHAT11790 <rahatsarker224@gmail.com>
2025-12-15 20:54:39 +0600

    Update info.py

--- info.py
+++ info.py
@@ -31,6 +31,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://RAHAT1132:RAHAT11a@rah
 DATABASE_NAME = environ.get('DATABASE_NAME', "rs_fllink_bot")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
+SHORTLINK = "False" # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2'
