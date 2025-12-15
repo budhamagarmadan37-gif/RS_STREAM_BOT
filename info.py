82cccdb422b08757b1569893c4b6f826588f7324
RAHAT11790 <rahatsarker224@gmail.com>
2025-12-15 19:13:09 +0600

    Upload info.py

---
+++ info.py
@@ -0,0 +1,36 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+# Bot information
+SESSION = environ.get('SESSION', 'Rs_link_bots')
+API_ID = int(environ.get('API_ID', '25976192'))
+API_HASH = environ.get('API_HASH', '8ba23141980539b4896e5adbc4ffd2e2')
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
+
+# Bot settings
+PORT = environ.get("PORT", "8080")
+
+# Online Stream and Download
+MULTI_CLIENT = False
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+if 'DYNO' in environ:
+    ON_HEROKU = True
+else:
+    ON_HEROKU = False
+URL = environ.get("URL", "")
+
+# Admins, Channels & Users
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003525738568'))
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6621572366').split()]
+
+# MongoDB information
+DATABASE_URI = environ.get('DATABASE_URI', "Rahatmongodb+srv://RAHAT1132:RAHAT11a@rahat.txn4lkk.mongodb.net/?appName=Rahat")
+DATABASE_NAME = environ.get('DATABASE_NAME', "rs_fllink_bot")
+
+# Shortlink Info
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
